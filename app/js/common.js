b8cb3247bc8469528e2b484f9204eb4c6e92d08c
Alexandr Vasilevskiy <vasilevskiy.alexandr@gmail.com>
2019-03-21 12:12:43 +0200

    add font-awesome, add owl-carousel(plus carousel styles)

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,10 @@
 $(function() {
 
-	// Custom JS
+	$('.owl-carousel').owlCarousel({
+		items: 3,
+		loop: true,
+		nav: true,
+		navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+	});
 
 });
